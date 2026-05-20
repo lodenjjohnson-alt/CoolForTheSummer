f05250d85951d355f757bcb6eb8ecfe4a9822576
lodenjjohnson-alt <loden.j.johnson@gmail.com>
2026-05-19 20:52:09 -0400

    Load experiences module styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import "./discipline.css";
 import "./faith.css";
+import "./experiences.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
