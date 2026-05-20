de975b6882f8e726d70423f278a8caebf1d68d48
lodenjjohnson-alt <loden.j.johnson@gmail.com>
2026-05-19 21:25:50 -0400

    Load rewards styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import "./faith.css";
 import "./experiences.css";
 import "./reflection.css";
 import "./operationArchive.css";
+import "./rewards.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
